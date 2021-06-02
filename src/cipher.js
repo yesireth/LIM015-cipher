7b022775b96238f9bf3af0553027d05eb3dd2881
yesireth <yesireth88@gmail.com>
2021-06-02 10:09:51 -0500

    muestra en console.log entrada

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
-  // ...
+  encode:(txtllave,txtEntrada)=>{
+console.log(txtEntrada + txtllave);
+  }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
+let btnEncriptar = document.getElementById("fnEncriptar")
+let btnDesencriptar = document.getElementById("fnDesencriptar")
+function fnEncriptar(){
+    let txtEntrada = document.getElementById("txtcaja1").value;
+    let txtcaja2 = document.getElementById("txtcaja2");
+    let txtllave = document.getElementById("txtllave").value;
+ 
+    cipher.encode(txtllave, txtEntrada);
+}
+
+
+ btnEncriptar.addEventListener("click", fnEncriptar)
 
 console.log(cipher);
