505c8893b0ee041548b43bc743b62016dd87b159
yesireth <yesireth88@gmail.com>
2021-06-07 09:59:35 -0500

    muestra el codigo desifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,49 @@
-const cipher = { encode:(txtllave,txtEntrada)=>{
-//INICIO FUNCION
-console.log(txtEntrada + txtllave);
-   let entrada = txtcaja1.value;
-   // entrada ------> AMOR
+const cipher = {
+  encode: (txtllave, txtEntrada) => {
+    //INICIO FUNCION
+    let salida="";
+    console.log(txtEntrada + txtllave);
+    let entrada = txtcaja1.value;
 
-    let array_caracteres=[entrada.length]
+    //entrada ------> AMOR
 
-    for (let i=0; i<entrada.length; i++){
-        console.log(entrada[i].charCodeAt()-64);
-        array_caracteres[i]=entrada[i].charCodeAt()-64;
+    let array_caracteres = [entrada.length]
+
+    for (let i = 0; i < entrada.length; i++) {
+
+      let posicion_act = entrada[i].charCodeAt();
+      posicion_act = (posicion_act - 65 + txtllave) % 26 + 65;
+
+      array_caracteres= posicion_act;
+      console.log(posicion_act);
+      
+      salida = salida+ String.fromCharCode(posicion_act);
+      //salida=salida+
+
     }
-
-    console.log(array_caracteres)
-
-    //FIN FUNCION
+ 
+    return salida;
+    
   }
-};
+}
+
+
+/*/ variable contenedora de letras codificadas
+var textConvert = [];
+// itera en textArr
+for (var number = 0; number < posicion_act.length; number++) {
+  // si el índice es = a espacio, mételo a textConvert
+  if (posicion_act[number] === ' ') {
+    textConvert.push(posicion_act[number]);
+  // si no, cámbialo de letra a número.
+  } else {
+    textConvert.push(String.fromCharCode(posicion_act[number]));
+  }
+
+  var textJoin = textConvert.join('');
+
+  
+}
+   return textJoin;*/
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import cipher from './cipher.js';
+
 let btnEncriptar = document.getElementById("fnEncriptar")
 let btnDesencriptar = document.getElementById("fnDesencriptar")
 
 function fnEncriptar(){
     let txtEntrada = document.getElementById("txtcaja1").value;
     let txtcaja2 = document.getElementById("txtcaja2");
-    let txtllave = document.getElementById("txtllave").value;
- 
-    cipher.encode(txtllave, txtEntrada);
+    let txtllave = parseInt(document.getElementById("txtllave").value);
+    
+
+    let resultado= cipher.encode(txtllave, txtEntrada);
+
+    txtcaja2.value=resultado;
 }
-
-
-
 
 function fnDesencriptar(){
     let txtcaja2 = document.getElementById("txtcaja2").value;
     let txtllave = document.getElementById("txtllave").value;
-   
+  
 }
 
-btnEncriptar.addEventListener("click", fnEncriptar)
+
+
+btnEncriptar.addEventListener("click", fnEncriptar);
 btnDesencriptar.addEventListener("click",btnDesencriptar);
