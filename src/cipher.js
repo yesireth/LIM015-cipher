d3c0cac772b6c62e37cf28d150c603c56eff62dd
yesireth <yesireth88@gmail.com>
2021-06-08 22:07:37 -0500

    muestra el texto descifrado y cifrado con el espacio y simbolo

--- src/cipher.js
+++ src/cipher.js
@@ -1,50 +1,46 @@
 const cipher = {
   encode: (txtllave, txtEntrada) => {
-    //INICIO FUNCION
+    
     let salida="";
-    console.log(txtEntrada + txtllave);
-    let entrada = txtcaja1.value;
-
-    //entrada ------> AMOR
-
-    let array_caracteres = [entrada.length]
+    let entrada=txtEntrada.toUpperCase();
+    console.log(entrada + txtllave);
 
     for (let i = 0; i < entrada.length; i++) {
 
-
       let posicion_act = entrada[i].charCodeAt();
-
-      posicion_act = (posicion_act - 65 + txtllave) % 26 + 65;
-
-      array_caracteres= posicion_act;
-      console.log(posicion_act);
       
-      salida = salida+ String.fromCharCode(posicion_act);
-      //salida=salida+
+      if(posicion_act<65 || posicion_act>90){
+      
+        salida= salida+entrada[i];
+      }
+      else{
+        posicion_act = (posicion_act - 65 + txtllave) % 26 + 65;
+        salida = salida+ String.fromCharCode(posicion_act);
+      }
+
+
     }
     return salida;
-    
   },
   decode: (txtllave, txtEntrada) => {
     let salida="";
+    let entrada=txtEntrada.toUpperCase();
     console.log(txtEntrada + txtllave);
 
-    for (let i = 0; i < txtEntrada.length; i++) {
-      let posicion_act = txtEntrada[i].charCodeAt();
-      posicion_act = (posicion_act + 65- txtllave) % 26 + 65;
-
+    for (let i = 0; i < entrada.length; i++) {
+      let posicion_act = entrada[i].charCodeAt();
+      if(posicion_act<65 || posicion_act>90){
+        salida= salida+entrada[i];
+      }
+      else{
+        posicion_act = (posicion_act + 65- txtllave) % 26 + 65;
+        salida = salida+ String.fromCharCode(posicion_act);
+      }
       console.log(posicion_act);
-      
-      salida = salida+ String.fromCharCode(posicion_act);
     }
     return salida;
    }
 
 }
- 
-
-
-
-
 
 export default cipher;
